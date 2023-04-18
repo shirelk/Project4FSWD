2db28ab8aac49a31e0f6590ce729c0d89c147428
Shirel <shkadosh@g.jct.ac.il>
2023-04-18 14:35:31 +0300

    numbers input, and visibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import "./App.css";
 
 function App(props) {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   const EngAlphabet1 = "qwertyuiop";
   const EngAlphabet2 = "asdfghjkl";
@@ -9,11 +10,17 @@ function App(props) {
   const HebAlphabet1 = "קראטוןםפ";
   const HebAlphabet2 = "שדגכעיחלךף";
   const HebAlphabet3 = "זסבהנמצתץ";
+  const numbers = "0123456789";
 
   const handleClick = (char) => {
     props.setInputValue(props.inputValue + char);
   };
 
+  const NumKeys = numbers.split("").map((char) => (
+    <button key={char} onClick={() => handleClick(char)}>
+      {char}
+    </button>
+  ));
   const EngKeys1 = EngAlphabet1.split("").map((char) => (
     <button key={char} onClick={() => handleClick(char)}>
       {char}
@@ -50,15 +57,59 @@ function App(props) {
       <h1>Text editor :)</h1>
       <div className="card">
         <input type="text" value={props.inputValue} />
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <div>{EngKeys1}</div>
-        <div>{EngKeys2}</div>
-        <div>{EngKeys3}</div>
-        <div>{HebKeys1}</div>
-        <div>{HebKeys2}</div>
-        <div>{HebKeys3}</div>
+        </button> */}
+        <div className="KeyboardOptions">
+          <button
+            onClick={() => {
+              const hebKeyboard = document.querySelector(".HebKeyboard");
+              hebKeyboard.style.visibility =
+                hebKeyboard.style.visibility === "collapse"
+                  ? "visible"
+                  : "collapse";
+            }}
+          >
+            Hebrew
+          </button>
+          <button
+            onClick={() => {
+              const engKeyboard = document.querySelector(".EngKeyboard");
+              engKeyboard.style.visibility =
+                engKeyboard.style.visibility === "collapse"
+                  ? "visible"
+                  : "collapse";
+            }}
+          >
+            English
+          </button>
+          <button
+            onClick={() => {
+              const numbersKeyboard =
+                document.querySelector(".NumbersKeyboard");
+              numbersKeyboard.style.visibility =
+                numbersKeyboard.style.visibility === "collapse"
+                  ? "visible"
+                  : "collapse";
+            }}
+          >
+            Numbers
+          </button>
+        </div>
+        <div className="NumbersKeyboard" style={{ visibility: "visible" }}>
+          {NumKeys}
+        </div>
+        <div className="EngKeyboard" style={{ visibility: "visible" }}>
+          <div>{EngKeys1}</div>
+          <div>{EngKeys2}</div>
+          <div>{EngKeys3}</div>
+        </div>
+        <div className="HebKeyboard" style={{ visibility: "visible" }}>
+          <div>{HebKeys1}</div>
+          <div>{HebKeys2}</div>
+          <div>{HebKeys3}</div>
+        </div>
+
         <div className="output">{props.output}</div>
 
         <p>
