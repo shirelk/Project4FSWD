193af2211fa862973b02231b81256411f102ddcb
Shirel <shkadosh@g.jct.ac.il>
2023-04-21 17:00:01 +0300

    ori's changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      capsLockOn: false,
+      // capsLockOn: false,
+      bold: false,
+      underline: false,
       inputValue: "",
       output: "",
     };
@@ -39,11 +41,14 @@ export default class App extends Component {
         {char}
       </button>
     ));
+  
+    
 
   render() {
     const { capsLockOn } = this.state;
     //const { inputValue } = this.props;
-    const { inputValue, output } = this.state;
+    const { inputValue, output, bold } = this.state;
+    // const { inputValue, output } = this.state;
     const EngAlphabet1 = capsLockOn ? "QWERTYUIOP" : "qwertyuiop";
     const EngAlphabet2 = capsLockOn ? "ASDFGHJKL" : "asdfghjkl";
     const EngAlphabet3 = capsLockOn ? "ZXCVBNM" : "zxcvbnm";
@@ -66,13 +71,72 @@ export default class App extends Component {
     //const emojyKeys1 = this.inputButtons(emojy1);
     const hebKeyboard = document.querySelector(".HebKeyboard");
     const engKeyboard = document.querySelector(".EngKeyboard");
-
+  
     return (
       <div className="App">
         {/* <h1>Text editor :)</h1> */}
         <div className="card">
-          <input className="input" type="text" value={inputValue} />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+          {/* <textarea className="container" value={inputValue} /> */}
 
+        <div class="container">
+          <div class="options">
+                    {/* fontAwesomes icons */}
+                {/* Text format */}
+                <button id="bold" className="option-button format"> <i class="fa-solid fa-bold"></i></button>
+                <button id="italic" className="option-button format"> <i class="fa-solid fa-italic"></i></button>
+                <button id="underline" className="option-button format"><i class="fa-solid fa-underline"></i></button>
+                <button id="strikethrough" class="option-button format"><i class="fa-solid fa-strikethrough"></i></button>
+                <button id="superscript" class="option-button script"><i class="fa-solid fa-superscript"></i></button>
+                <button id="subscript" class="option-button script"><i class="fa-solid fa-subscript"></i></button>
+
+              {/*List style*/} 
+              <button id="insertOrderedList" class="option-button">
+                <div class="fa-solid fa-list-ol"></div>
+              </button>
+              <button id="insertUnorderedList" class="option-button"><i class="fa-solid fa-list"></i></button>
+              
+              { /*Undo-Redo*/}
+              <button id="uno" class="option-button"><i class="fa-solid fa-rotate-left"></i></button>
+              <button id="redo" class="option-button"><i class="fa-solid fa-rotate-right"></i></button>
+
+              {/* Alignment */}
+              <button id="justigyLeft" class="option-button align"><i class=" fa-solid fa-align-left"></i></button>
+              <button id="justigyRight" class="option-button align"><i class=" fa-solid fa-align-right"></i></button>
+              <button id="justigyLeft" class="option-button align"><i class=" fa-solid fa-align-center"></i></button>
+              <button id="justigyFull" class="option-button align"><i class=" fa-solid fa-align-justify"></i></button>
+              <button id="indent" className="option-button spacing"><i class="fa-solid fa-indent"></i></button>
+              <button id="outdent" className="option-button spacing"><i class="fa-solid fa-outdent"></i></button>
+
+              {/* Headings */}
+              <select id="formatBlock" class="adv-option-button">
+                <option value="H1">H1</option>
+                <option value="H2">H2</option>
+                <option value="H3">H3</option>
+                <option value="H4">H4</option>
+                <option value="H5">H5</option>
+                <option value="H6">H6</option>
+              </select>
+
+              {/* Font */}
+              <select id="fontName" class="adv-option-button"></select>
+              <select id="fontSize" class="adv-option-button"></select>
+
+              {/* Color */}
+              <div class="input-wrapper">
+                <input type="color" id="foreColor" class="adv-option-button"></input>
+                <label for="foreColor">Font Color</label>
+              </div>
+              <div class="input-wrapper">
+                <input type="color" id="backColor" class="adv-option-button"></input>
+                <label for="backColor">Hightlight Color</label>
+              </div>
+            </div>
+          <div id="text-input" contentEditable="true"></div>
+          <script src="script.js"></script>
+        </div>
+
+          {/* keyboard */}
           <div className="KeyboardInputs">
             <button
               onClick={() => {
@@ -135,9 +199,14 @@ export default class App extends Component {
             <div>{emojyKeys1}</div>
           </div> */}
 
-          <div className="output">{output}</div>
+     
+
+          {/* <div className="output">{output}</div> */}
+        </div> 
         </div>
-      </div>
+      
     );
-  }
-}
+        }
+      }
+      
+  
